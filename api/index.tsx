9419524c791db2b5e1d7c659631162f7066bbf02
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-09-13 20:02:35 -0500

    saedsg

--- api/index.tsx
+++ api/index.tsx
@@ -276,12 +276,11 @@
             {errorMessage ? (
               <p style={{ fontSize: '55px', color: 'red', textShadow: '1px 1px 2px rgba(0,0,0,0.5)' }}>Error: {errorMessage}</p>
             ) : userInfo ? (
-              <div style={{ display: 'flex', width: '100%', justifyContent: 'space-around', position: 'absolute', top: '46%' }}>
+              <div style={{ display: 'flex', width: '100%', justifyContent: 'center', position: 'absolute', top: '46%' }}>
                 <p style={{ 
                   fontSize: '60px', 
                   fontWeight: 'bold', 
-                  color: '#FFFFFF',
-                  textAlign: 'center'
+                  color: '#FFFFFF'
                 }}>
                   {Number(userInfo.todayEarnings).toFixed(2)}
                 </p>
