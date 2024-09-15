4f8ab9c5a39aefc9fd815f0fe58342505baab8ad
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-09-15 18:48:43 -0500

    hrhe5

--- api/index.tsx
+++ api/index.tsx
@@ -158,14 +158,19 @@ async function getMoxieUserInfo(fid: string): Promise<MoxieUserInfo> {
 }
 
 app.frame('/', (c) => {
+  // Replace this URL with the actual Giphy embed URL you received
+  const giphyEmbedUrl = 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmM4Z2IyZndodXgwODBleG45MnlxeWNuOXRnY3JxNjh4ZTVvdjNzeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/32ou4q9dHMFynb1jjF/giphy.gif';
+  
   return c.res({
     image: (
-      <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
-        <img 
-          src="https://bafybeieo7vvxff3xadbfaylxdrk5rqkadf23bou2nj6aunakitxvdtp47i.ipfs.w3s.link/IMG_7916%201.gif"
-          alt="Animated GIF"
-          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-        />
+      <div style={{ display: 'flex', width: '100%', height: '100%' }}>
+        <iframe 
+          src={giphyEmbedUrl} 
+          width="100%" 
+          height="100%" 
+          style={{ border: 'none', pointerEvents: 'none' }}
+          allowFullScreen
+        ></iframe>
       </div>
     ),
     intents: [
