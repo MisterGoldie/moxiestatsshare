0c62950553f288b24e7927b4b40c2af53cac92a5
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2024-09-15 08:57:43 -0500

    fbdrsw

--- api/index.tsx
+++ api/index.tsx
@@ -158,32 +158,19 @@ async function getMoxieUserInfo(fid: string): Promise<MoxieUserInfo> {
 }
 
 app.frame('/', (c) => {
-  const gifUrl = 'https://bafybeieo7vvxff3xadbfaylxdrk5rqkadf23bou2nj6aunakitxvdtp47i.ipfs.w3s.link/IMG_7916%201.gif';
+  const gifUrl = 'https://bafybeieo7vvxff3xadbfaylxdrk5rqkadf23bou2nj6aunakitxvdtp47i.ipfs.w3s.link/IMG_7916%201.gif'
   
   return c.res({
     image: (
-      <div style={{
-        display: 'flex',
-        width: '100%',
-        height: '100%',
-        overflow: 'hidden',
-      }}>
-        <img 
-          src={gifUrl} 
-          alt="Animated background"
-          style={{
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
-          }}
-        />
+      <div style={{ display: 'flex', width: '100%', height: '100%' }}>
+        <img src={gifUrl} alt="Animated background" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
       </div>
     ),
     intents: [
       <Button action="/check">Check stats</Button>,
     ],
-  });
-});
+  })
+})
 
 app.frame('/check', async (c) => {
   console.log('Entering /check frame');
